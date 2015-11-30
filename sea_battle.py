03b7b10b05126cfc3ff296c9597d294ca47a1c80
Alexsandra <trubetskaya.cyb@gmail.com>
2015-11-30 22:45:09 +0200

    Валидация координат

--- sea_battle.py
+++ sea_battle.py
@@ -5,6 +5,12 @@ SHIP = 1
 
 board = [[EMPTY_CELL] * BOARD_SIZE for i in range(BOARD_SIZE)]
 
+# Согласно википедии: "Горизонтали обычно нумеруются сверху вниз, а вертикали помечаются буквами слева направо"
+# диапазон значений по х
+X_COORD_RANGE = 'abcdefghij'
+# диапазон значений по у
+Y_COORD_RANGE = range(1, 11)
+
 
 def add_ship(board, x, y):
     """
@@ -15,3 +21,35 @@ def add_ship(board, x, y):
     1
     """
     board[x][y] = SHIP
+
+def validate_coord(coord):
+    """
+    проверка правильности координат (формат "MK", где M - в X_COORD_RANGE, K - в Y_COORD_RANGE)
+    >>> coord = 'a1'
+    >>> validate_coord(coord)
+    True
+    >>> coord = 'j10'
+    >>> validate_coord(coord)
+    True
+    >>> coord = 'E5' # кейс с координатой с заглавной буквой
+    >>> validate_coord(coord)
+    True
+    >>> coord =  True
+    >>> validate_coord(coord)
+    False
+    >>> coord =  ['a', '1']
+    >>> validate_coord(coord)
+    False
+    >>> coord =  'a11'
+    >>> validate_coord(coord)
+    False
+    >>> coord =  'z1'
+    >>> validate_coord(coord)
+    False
+    """
+    is_valid = False
+    try:
+        if coord[0].lower() in X_COORD_RANGE and Y_COORD_RANGE.index(int(coord[1:])) + 1:
+            is_valid = True
+    finally:
+        return is_valid
