7c474b708a409c6d112dcafdeb4e803cdd3fe94d
serhiidmytrenko <dmytrenko.serhii@gmail.com>
2015-12-07 03:26:44 +0200

    Complite task Перевод координат #2

--- sea_battle.py
+++ sea_battle.py
@@ -45,3 +45,28 @@ def validate_coord(coord):
     except (ValueError, TypeError):
         return False
     return 'a' <= letter <= 'j' and 1 <= number <= 10
+
+"""
+Для перевода формата нам нужны выдаленые ранее константы, которые
+обозначают номерацию поля
+"""
+X_COORD_RANGE = list('abcdefghij') # диапазон значений по x
+Y_COORD_RANGE = range(1, 11) ## диапазон значений по y
+
+
+def transform_coord(coord):
+    """
+    Функция переводит полученое значение выстрела формата "МК" в формат
+    координат по оси x и y
+    >>> transform_coord('d5')
+    (3, 4)
+    >>> transform_coord('e7')
+    (4, 6)
+    >>> transform_coord('a1')
+    (0, 0)
+    >>> transform_coord('j10')
+    (9, 9)
+    """
+    x_coord_shoot = X_COORD_RANGE.index(coord[0]) #Перевод буквы координаты Х в индекс
+    y_coord_shoot = Y_COORD_RANGE.index(int(coord[1:])) #Перевод числа координаты Y в индекс
+    return x_coord_shoot, y_coord_shoot
